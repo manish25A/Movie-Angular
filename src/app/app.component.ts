8e9a6ef2712bc4c7651e11cce6f30f6c00f9a37a
Manish B.C <49949864+manish25A@users.noreply.github.com>
2021-07-28 09:04:28 +0545

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'video-game-db';
+  title = 'this is video game database in angular';
 }
